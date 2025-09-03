3bf73e72a5f1366dd4ab29c511d6b0c584d19d92
Buford Eeds <bufordeeds8@gmail.com>
2025-09-02 21:57:22 -0500

    feat: Add migration index file to manage database migrations

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20250903_024759 from './20250903_024759';
+
+export const migrations = [
+  {
+    up: migration_20250903_024759.up,
+    down: migration_20250903_024759.down,
+    name: '20250903_024759'
+  },
+];
